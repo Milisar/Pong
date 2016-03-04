baece56ee503e59d6652ff0c5897827c6caafefb
Milisar <4240@pierson.nl>
2016-03-04 12:08:13 +0100

    Player aanpassen
    weinig werk, heb oortjes nodig en snel

--- Player.java
+++ Player.java
@@ -1,5 +1,24 @@
 package Pong;
 
+import java.awt.Graphics;
+
 public class Player {
 
+	private double x;
+	private double y;
+	
+	public Player(double x, double y){
+		this.x =x;
+		this.y = y;
+		
+	}
+	
+	public void tick(){
+		
+	}
+	
+	public void render(Graphics g){
+		
+	}
+	
 }
